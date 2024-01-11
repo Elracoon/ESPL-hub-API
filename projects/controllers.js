861d54921cc49ce9b88a96cae02c99cb5100b1ab
leanecharpentier <leanecharpentierpro@outlook.com>
2024-01-11 16:46:06 +0100

    routes projects

--- projects/controllers.js
+++ projects/controllers.js
@@ -1,11 +1,20 @@
 import Db from '../config/db.js'
+import { ObjectId } from 'mongodb';
 
 export async function updateProject(req, res) {
     res.status(200).send("update project")
 }
 
 export async function getAllProject(req, res) {
-    const db = Db.connect();
-    const result = db.collection("project").find();
-    res.status(200).send("result")
+    const db = await Db.connect();
+    const result = await db.collection('projects').find({}).toArray();
+    res.status(200).send(result)
+}
+
+export async function getOneProject(req, res) {
+    const projectId = req.params.id
+    const db = await Db.connect();
+    const result = await db.collection('projects').findOne({_id: new ObjectId(projectId)});
+    console.log(result)
+    res.status(200).send(result)
 }
--- projects/routes.js
+++ projects/routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
-import { getAllProject, updateProject } from "./controllers.js"
+import { getAllProject, getOneProject, updateProject } from "./controllers.js"
 
 const router = Router()
 
 router.get("/", getAllProject)
+router.get("/:id", getOneProject)
 // router.post('/add', createProject)
 router.patch('/update', updateProject)
 
