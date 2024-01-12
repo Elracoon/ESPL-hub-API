b786939b579dc52906972fb8ae689784c41eec02
leanecharpentier <leanecharpentierpro@outlook.com>
2024-01-12 12:01:29 +0100

    models project

---
+++ projects/models.js
@@ -0,0 +1,33 @@
+import { Schema , model } from 'mongoose';
+
+const projectSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    projectManager: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true
+    },
+    competences: {
+        type: String,
+        enum: ['developpement', 'design', 'marketing', 'communciation', 'ux/ui'],
+        default: 'A contacter'
+    },
+    members: [{
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    }]
+});
+
+const Project = model('projects', projectSchema);
+
+export default Project;  
